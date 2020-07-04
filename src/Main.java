67335a36dbfb41fb04aaf2ca1c4154fc89ebd1e6
DİYAR <diyarbjk44@gmail.coo>
2020-07-04 16:13:16 +0300

    multiplication table

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,45 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("<---------->");
-for (int i=0; i<=10; i++){
-    System.out.println("+\t+");
-    System.out.println("  +");
+        System.out.println();
+for (int a=1; a<=10; a++){
+    System.out.print(a+"\t");
 }
-System.out.println("<---------->");
+System.out.println();
+for (int b=1; b<=10; b++){
+    System.out.print(b*2+"\t");
+}
+System.out.println();
+for (int c=1; c<=10; c++){
+    System.out.print(c*3+"\t");
+}
+System.out.println();
+for (int d=1; d<=10; d++){
+    System.out.print(d*4+"\t");
+}
+System.out.println();
+for (int e=1; e<=10; e++){
+    System.out.print(e*5+"\t");
+}
+System.out.println();
+for (int f=1; f<=10; f++){
+    System.out.print(f*6+"\t");
+}
+System.out.println();
+for (int g=1; g<=10; g++){
+    System.out.print(g*7+"\t");
+}
+System.out.println();
+for (int h=1; h<=10; h++){
+    System.out.print(h*8+"\t");
+}
+System.out.println();
+for (int i=1; i<=10; i++){
+    System.out.print(i*9+"\t");
+}
+System.out.println();
+for (int j=1; j<=10; j++){
+    System.out.print(j*10+"\t");
+}
     }
 }
