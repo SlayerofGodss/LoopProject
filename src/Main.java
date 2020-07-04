7e4c3bc4ab6b2eb3d148c36adf06359f8cbbfd51
DİYAR <diyarbjk44@gmail.coo>
2020-07-04 13:46:49 +0300

    Fruit Loops :D

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("<---------->");
+for (int i=0; i<=10; i++){
+    System.out.println("+\t+");
+    System.out.println("  +");
+}
+System.out.println("<---------->");
+    }
+}
