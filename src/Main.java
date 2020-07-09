31f0d7317ec7b821d92d2964acf34127ea23c318
DİYAR <diyarbjk44@gmail.coo>
2020-07-09 15:48:01 +0300

    girilen sayıları toplama

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
-for (int a=1; a<=10; a++){
-    System.out.print(a+"\t");
-}
-System.out.println();
-for (int b=1; b<=10; b++){
-    System.out.print(b*2+"\t");
-}
-System.out.println();
-for (int c=1; c<=10; c++){
-    System.out.print(c*3+"\t");
-}
-System.out.println();
-for (int d=1; d<=10; d++){
-    System.out.print(d*4+"\t");
-}
-System.out.println();
-for (int e=1; e<=10; e++){
-    System.out.print(e*5+"\t");
-}
-System.out.println();
-for (int f=1; f<=10; f++){
-    System.out.print(f*6+"\t");
-}
-System.out.println();
-for (int g=1; g<=10; g++){
-    System.out.print(g*7+"\t");
-}
-System.out.println();
-for (int h=1; h<=10; h++){
-    System.out.print(h*8+"\t");
-}
-System.out.println();
-for (int i=1; i<=10; i++){
-    System.out.print(i*9+"\t");
-}
-System.out.println();
-for (int j=1; j<=10; j++){
-    System.out.print(j*10+"\t");
-}
+        int giris, kacAdet, Toplam=0;
+        Scanner sayiAl = new Scanner(System.in);
+
+        System.out.println("Kaç Adet Sayı gireceksiniz?");
+        kacAdet = sayiAl.nextInt();
+        int[] girilenSayi = new int[kacAdet];
+
+        for (int x = 0; x < girilenSayi.length; x++) {
+            System.out.println("Tam Sayi Giriniz:");
+            giris = sayiAl.nextInt();
+            girilenSayi[x] = giris;
+            Toplam = Toplam+ girilenSayi[x];
+        }
+
+        for (int x = 0; x < girilenSayi.length; x++) {
+            System.out.println("girilenSayi[" + x + "]=" + girilenSayi[x]);
+        }
+
+        System.out.println("Toplamı:" + Toplam);
     }
 }
+
